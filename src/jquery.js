115d89658335faa1d3747d685f5b7974eb8d8b6a
draganet <3417237276@qq.com>
2021-09-12 02:50:50 +0800

    init

--- src/jquery.js
+++ src/jquery.js
@@ -1,5 +1,10 @@
-window.jQuery = function (selector) {
-    const elements = document.querySelectorAll(selector);
+window.jQuery = function (selectorOrArray) {
+    let elements;
+    if (typeof selectorOrArray === 'string') {
+        elements = document.querySelectorAll(selectorOrArray);
+    } else if (selectorOrArray instanceof Array) {
+        elements = selectorOrArray;
+    }
     // api 可以操作 elements
     return {
         // 闭包： 函数访问外部变量
@@ -10,14 +15,47 @@ window.jQuery = function (selector) {
             return this;
         },
         find(selector) {
-            console.log(2);
             let array = [];
             for (let i = 0; i < elements.length; i++) {
                 // const elements2 = Array.from(elements[i].querySelectorAll(selector));
                 // array = array.concat(elements2);
                 array = array.concat(Array.from(elements[i].querySelectorAll(selector)));
             }
-            return array;
+            // return this; 有个小 bug，不能返回 this
+            // const newApi = jQuery(array);
+            // return newApi;
+            array.oldApi = this; // this 是旧的 api
+            return jQuery(array);
+        },
+        oldApi: selectorOrArray.oldApi,
+        end() {
+            return this.oldApi;
+        },
+        each(fn) {
+            for (let i = 0; i < elements.length; i++) {
+                fn.call(null, elements[i], i);
+            }
+            return this;
+        },
+        parent() {
+            const array = []
+            this.each((node) => {
+                if (array.indexOf(node.parentNode) === -1) {
+                    array.push(node.parentNode);
+                }
+            })
+            return jQuery(array);
+        },
+        print() {
+            console.log(elements);
+        },
+        children() {
+            const array = [];
+            this.each((node) => {
+                array.push(...node.children); // 展开操作符
+                // array.push(node.children[0], node.children[1], node.children[2]);
+            })
+            return jQuery(array);
         }
     }
 }
